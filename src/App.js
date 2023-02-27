24069bae9e937f4b6a951fe5235474cfc9929401
Tae Hyeon Jo <hillskuti@naver.com>
2023-02-27 22:46:58 +0900

    Test Initial Condition of Button and Checkbox

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
   return (
     <div>
       <button
