38e4e1a97e5ef373b24c273bb73fc55da16e77af
Tae Hyeon Jo <hillskuti@naver.com>
2023-03-01 08:06:44 +0900

    Finding Checkbox with Label

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
   const [disabled, setDisabled] = useState(false);
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
   return (
     <div>
       <button
@@ -21,6 +22,7 @@ function App() {
         defaultChecked={disabled}
         onChange={(e) => setDisabled(e.target.checked)}
       />
+      <label htmlFor='disable-button-checkbox'>Disable button</label>
     </div>
   );
 }
